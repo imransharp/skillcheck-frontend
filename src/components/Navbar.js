8b26e1d210c45f92ce835e31ff9f3da53b18e74b
Imran Bhatti <imranb.bhatti@gmail.com>
2025-07-23 16:03:52 +0500

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">SkillCheck</div>
+      <ul className="navbar-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/dashboard">Dashboard</Link></li>
+        <li><Link to="/assessments">Assessments</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;

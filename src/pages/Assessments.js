38edcf3e4768a63c59ae713481935defb08934c6
Imran Bhatti <imranb.bhatti@gmail.com>
2025-07-23 15:57:18 +0500

    Add files via upload

---
+++ src/pages/Assessments.js
@@ -0,0 +1,10 @@
+function Assessments() {
+  return (
+    <div>
+      <h2>Assessments</h2>
+      <p>Take a quiz to test your knowledge.</p>
+    </div>
+  );
+}
+
+export default Assessments;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to SkillCheck 🧠</h1>
+      <p>Your journey to mastering skills starts here.</p>
+    </div>
+  );
+}
+
+export default Home;

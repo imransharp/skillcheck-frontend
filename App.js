f8734381a95d5c32bfae805c68f18f939b823a77
Imran Bhatti <imranb.bhatti@gmail.com>
2025-07-23 16:03:22 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Assessments from './pages/Assessments';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/assessments" element={<Assessments />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
